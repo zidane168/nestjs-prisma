9460414fcb589fe987035810ff96ab6e6b081a1c
Vi Ly <vi.lh@>
2024-11-25 17:20:22 +0700

    - update db with password 1->6

---
+++ db.sql
@@ -0,0 +1,5 @@
+ 
+INSERT INTO `Administrator` (`id`, `name`, `email`, `password`, `phone`, `enabled`, `lastLoggedIn`, `created`, `updated`, `slug`) VALUES
+(1, 'admin', 'admin@gmail.com', '$2b$10$qTNOn.2T4JdWG1S3hbj85uutbfBe68UMumJwFvVGzoTDXmLW3GuEy', '0906440368', 1, '2024-11-25 15:39:52.000', '2024-11-25 15:39:52.000', NULL, 'super-admin'),
+(3, 'test', 'test@gmail.com', '$2b$10$qTNOn.2T4JdWG1S3hbj85uutbfBe68UMumJwFvVGzoTDXmLW3GuEy', '0906440369', 1, '2024-11-25 15:39:52.000', '2024-11-25 15:39:52.000', NULL, 'sale');
+COMMIT;
