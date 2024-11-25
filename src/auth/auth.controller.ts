c1daba027f76cb41778bcbedf0b03a66111517a6
Vi Ly <vi.lh@>
2024-11-25 17:34:36 +0700

    - tai sao local strategy bi loi!!

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
   
   @Post('login')
+  @UseGuards(AuthGuard('local'))
   async login(@Body() body: { email: string; password: string }) { 
     return this.authService.login(body.email, body.password);
   }
---
+++ src/auth/strategies/local.strategy.ts
@@ -0,0 +1,22 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common'; 
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+import { AuthService } from '../auth.service';
+
+// https://www.bing.com/videos/riverview/relatedvideo?&q=Nest+JS+JWT+Authentication&&mid=D6BBC2D7AD602E62B5CBD6BBC2D7AD602E62B5CB&&FORM=VRDGAR
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super() 
+  }
+
+  async validate(email: string, password: string) {
+    const user = await this.authService.validateUser(email, password)
+
+    if (!user)  {
+        throw new UnauthorizedException()
+    }
+
+    return user; 
+  }
+}
